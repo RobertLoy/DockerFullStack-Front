70eca1d76849c90bd22364eb1bf1399dc2d38a32
RobertLoy <robert.loy@gcu.edu>
2021-04-22 13:47:52 -0700

    First Commit

---
+++ todo-app/build/precache-manifest.bebe669e8d3b6c9aa91058e04292aa3c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "23c4d9cfbf34c31fdb70",
+    "url": "/static/css/main.1e4b56ce.chunk.css"
+  },
+  {
+    "revision": "23c4d9cfbf34c31fdb70",
+    "url": "/static/js/main.e3542cfa.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "4c4e45c21b9d5021aa11",
+    "url": "/static/js/2.aecb81c7.chunk.js"
+  },
+  {
+    "revision": "9308230be6f6ceebc65d65d981fe99ff",
+    "url": "/index.html"
+  }
+];
